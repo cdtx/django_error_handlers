aaa9e3078d41288fb50b469bb15963ee2aa6c25f
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-07-03 14:20:56 +0200

    Brutal commit of all the app...

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='django_error_handlers',
+    version='0.0.1',
+    description='Custom views and pages for the common http errors',
+    author='cdtx',
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=['cdtx.django_error_handlers'],
+    install_requires = []
+)
+
+
