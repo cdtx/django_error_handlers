8f44f4be848f3490c506c3c7cd1d0e00110d68d2
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-07-05 16:10:23 +0200

    Updated view accordingly

--- cdtx/django_error_handlers/views.py
+++ cdtx/django_error_handlers/views.py
@@ -3,32 +3,34 @@ from django.shortcuts import render
 from django.http import *
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
+uniqueTemplate = 'django_error_handlers/error_xxx.html'
+
 # Create your views here.
 def handler400(request):
     context = RequestContext(request, {
                             'error_number': '400',
                             'image_path': static('images/bad_request_brian.jpg'),
                         })
-    return HttpResponseBadRequest(render(request, 'error_xxx.html', context_instance=context))
+    return HttpResponseBadRequest(render(request, uniqueTemplate, context_instance=context))
 
 def handler403(request):
     context = RequestContext(request, {
                             'error_number': '403',
                             'image_path': static('images/less_privileges.jpg'),
                         })
-    return HttpResponseForbidden(render(request, 'error_xxx.html', context_instance=context))
+    return HttpResponseForbidden(render(request, uniqueTemplate, context_instance=context))
 
 def handler404(request):
     context = RequestContext(request, {
                             'error_number': '404',
                             'image_path': static('images/confused_travolta.gif'),
                         })
-    return HttpResponseBadRequest(render(request, 'error_xxx.html', context_instance=context))
+    return HttpResponseBadRequest(render(request, uniqueTemplate, context_instance=context))
 
 def handler500(request):
     context = RequestContext(request, {
                             'error_number': '500',
                             'image_path': static('images/server_error.jpg'),
                         })
-    return HttpResponseServerError(render(request, 'error_xxx.html', context_instance=context))
+    return HttpResponseServerError(render(request, uniqueTemplate, context_instance=context))
 
